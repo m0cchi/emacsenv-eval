fc8e06ebc95b9455060cbd75bc948e43d761883f
m0cchi <boom.boom.planet@gmail.com>
2016-10-02 02:29:35 +0900

    eval plugin

---
+++ plugins/eval.sh
@@ -0,0 +1,19 @@
+RAND=/dev/urandom
+ls $RAND &> /dev/null
+
+if [ ! $? -eq 0 ]; then
+    RAND=/dev/random
+    ls $RAND &> /dev/null
+    if [ ! $? -eq 0 ]; then
+        exit 1
+    fi
+fi
+
+ELISP_CODE=$1
+shift 1
+
+TMP_FILENAME=$(cat $RAND | LC_CTYPE=C tr -dc '[:alnum:]' | head -c 40)
+TMP_FILENAME=/tmp/emacsenv_$TMP_FILENAME.el
+echo $ELISP_CODE > $TMP_FILENAME
+emacs --script $TMP_FILENAME
+rm -f $TMP_FILENAME
